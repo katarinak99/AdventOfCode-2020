830dbb41bd3527784806370b2040992d169daf7a
Katarina <katarinca61@gmail.com>
2022-10-01 17:34:40 +0200

    Add Day 4

---
+++ Day04/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Day04
+{
+    class Program
+    {
+        private static readonly List<string> RequiredFields = new()
+        {
+            "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"
+        };
+
+        private static void Main()
+        {
+            var input = File.ReadAllText("input04.txt");
+            var passports = input.Split("\n\n");
+
+            var result = passports
+                .Count(passport => RequiredFields.All(passport.Contains));
+            
+            Console.WriteLine(result);
+        }
+    }
+}
